554681deafcc2b98bbeaf4743f1c43245e9d9302
Andrei Ionita <gogu.samsung@gmail.com>
2020-06-11 20:53:11 +0300

    Update dnamicva.js

--- dnamicva.js
+++ dnamicva.js
@@ -8,10 +8,8 @@ for (i=0; i<= slots.length-1; i++ )
 
 //set targeing keys push to googletag push queue
 
-<script> 
 
-
-googletag.cmd.push(function setva () {
+-googletag.cmd.push(function setva () {
   var slots = googletag.pubads().getSlots(); 
   var i; 
   for (i=0; i<= slots.length-1; i++ )  
@@ -20,6 +18,25 @@ googletag.cmd.push(function setva () {
   
 })
 
+<script> 
+
+// get the AdX CPM from json report and set the kv to an increased CPM by percentage
+for(j=0; j<=slots.length-1; j++) 
+	{if(slots[j].getAdUnitPath() !== null){
+		{ for(i=0; i<=json.length-1; i++) 
+			{
+			
+			if(json[i]['Ad unit'] == slots[j].getAdUnitPath()) 
+			{ console.log(slots[j].getAdUnitPath() + " has average AdX CPM of " + json[i]["Ad Exchange average eCPM"]);
+				var newCPM = json[i]["Ad Exchange average eCPM"]*1.1;
+				newCPM = newCPM.toFixed(2);
+				slots[j].setTargeting("eCPM", newCPM);
+				 console.log("becomes " + newCPM );
+				}}
+		}
+	}}	
+	
+	
 
 
  </script>
