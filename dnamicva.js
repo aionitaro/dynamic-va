d8a36a1d51060eb8d485da2c7c5416def352065f
Andrei Ionita <gogu.samsung@gmail.com>
2020-06-11 14:29:40 +0300

    Create dnamicva.js

---
+++ dnamicva.js
@@ -0,0 +1,25 @@
+
+//get targeing keys
+
+var slots = googletag.pubads().getSlots(); 
+var i; 
+for (i=0; i<= slots.length-1; i++ ) 
+	{ console.log(slots[i].getAdUnitPath() + " " + slots[i].getTargetingKeys()); }
+
+//set targeing keys
+
+<script> 
+
+
+googletag.cmd.push(function setva () {
+  var slots = googletag.pubads().getSlots(); 
+  var i; 
+  for (i=0; i<= slots.length-1; i++ )  
+	{ console.log("Setting viewbability for " + slots[i].getAdUnitPath() + " to " + i*2);
+      slots[i].setTargeting("viewability", i*2); }
+  
+})
+
+
+
+ </script>
